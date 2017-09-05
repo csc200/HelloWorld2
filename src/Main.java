2e6acca7f4c47ba77842a43ccaeebd8a584b4b61
Tanes Kanchanawanchai <tkanchanawanchai@nvcc.edu>
2017-09-05 17:26:39 -0400

    add javadoc

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,22 @@
+import java.util.Scanner;
 /**
  * Purpose: Display Hello World
  * @author Tanes Kanchanawanchai
+ * @version 1.0 9/5/2017
  */
-
-import java.util.Scanner;
 public class Main {
-
+    /**
+     * This is a main method for the application.
+     * @param args any string input
+     */
     public static void main(String[] args) {
+        //this is a scanner
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your name");
+        /*
+            using scanner to read a string input.
+            This sc is an object  of Scanner class
+         */
         String name = sc.next();
         System.out.println("Hello " + name);
     }
